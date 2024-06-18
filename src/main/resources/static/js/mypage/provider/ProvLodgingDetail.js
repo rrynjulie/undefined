556ee33ffe3fbdb59bf87b0fa1dbd968e03a1bd8
JaeHyeokee <jaehyeok817@naver.com>
2024-06-18 18:50:57 +0900

    이재혁_20240618_판매자숙소상세수정(imageslider삽입)

---
+++ src/main/resources/static/js/mypage/provider/ProvLodgingDetail.js
@@ -0,0 +1,36 @@
+let currentIndex = 0;
+showSlides(currentIndex);
+
+// 3초마다 페이지가
+setInterval(function() {
+    plusSlide(0);
+}, 3000);
+
+function plusSlide(idx) {
+    showSlides(currentIndex += idx);
+}
+
+function currentSlide(idx) {
+    showSlides(currentIndex = idx);
+}
+
+function showSlides(idx) {
+    const slides= document.querySelectorAll('.lodging-img');
+    const dots= document.querySelectorAll('.dot');
+
+    if (idx >= slides.length) {
+        currentIndex = 0;
+    } else if (idx < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = idx;
+    }
+
+    slides.forEach((slide, i) => {
+        slide.style.display = 'none';
+        dots[i].classList.remove('active');
+    });
+
+    slides[currentIndex].style.display = 'block';
+    dots[currentIndex].classList.add('active');
+}
