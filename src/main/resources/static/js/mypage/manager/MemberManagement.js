90264c06f2509ab091840c4ca8d656a326fa4a31
jeongjunyeong <nejunyoung@gmail.com>
2024-06-18 16:47:12 +0900

    정준영_20240618_html_css_js 수정1

---
+++ src/main/resources/static/js/mypage/manager/MemberManagement.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function (){
+    document.getElementById("lodging-update").addEventListener("click", function (){
+        window.location.href = "../../../../templates/views/mypage/manager/ApprovalLodgingUpdate.html"
+    });
+    document.getElementById("member-management").addEventListener("click", function (){
+        window.location.href = "../../../../templates/views/mypage/manager/MemberManagement.html"
+    });
+    document.getElementById("manage-info-btn").addEventListener("click", function (){
+        window.location.href = "../../../../templates/views/mypage/ManageAccount.html"
+    });
+});
