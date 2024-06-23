b3163423c7a8be8bc243d44348dc1bdec49d056f
yeongjae0113 <dudwo0113@gmail.com>
2024-06-23 19:52:42 +0900

    최영재_20240624_Lodging 여러가지 수정, ReviewCreate_html, js, css 파일생성

---
+++ src/main/resources/static/js/mypage/customer/ReviewCreate.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const stars = document.querySelectorAll('.star');
+    const ratingInput = document.getElementById('rating');
+
+    stars.forEach(star => {
+        star.addEventListener('click', function() {
+            const value = this.getAttribute('data-value');  // 'date-value' 값을 가져와서 value 에 저장
+            ratingInput.value = value;  // 선택된 value 에 따라 표시
+            highlightStars(value);  // highlightStars 에 저장
+        });
+
+        star.addEventListener('mouseover', function() { // 마우스를 star 에 올렸을 때
+            const value = this.getAttribute('data-value');
+            highlightStars(value);
+        });
+
+        star.addEventListener('mouseout', function() {  // 마우스를 star 밖으로 댈 때
+            const value = ratingInput.value;    // ratingInput 의 value 를 value 에 저장
+            highlightStars(value);
+        });
+    });
+
+    function highlightStars(value) {
+        stars.forEach(star => {
+            if (star.getAttribute('data-value') <= value) {   // 'date-vale' 값이 value 보다 작거나 같을 때 실행
+                star.classList.add('highlighted');  // highlighted 클래스 추가
+            } else {    // 클 때
+                star.classList.remove('highlighted');   // highlighted 클래스 제거
+            }
+        });
+    }
+});
