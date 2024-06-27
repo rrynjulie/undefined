8df8c6b133c372f216b2c2146b282f5fc3447449
JaeHyeokee <jaehyeok817@naver.com>
2024-06-27 13:55:20 +0900

    이재혁_20240627_부킹부킹서비스단수정

--- src/test/java/com/lec/spring/repository/BookingRepositoryTest.java
+++ src/test/java/com/lec/spring/repository/BookingRepositoryTest.java
@@ -1,15 +1,46 @@
 package com.lec.spring.repository;
 
+import com.lec.spring.domain.Booking;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @SpringBootTest
 public class BookingRepositoryTest {
     @Autowired
     private SqlSession sqlSession;
+
+    @Test
+    void bookingCreateTest() {
+        BookingRepository bookingRepository = sqlSession.getMapper(BookingRepository.class);
+        System.out.println("예약 생성");
+
+        Booking booking = Booking.builder()
+                .bookingTime(LocalDateTime.parse("2024-05-25T14:20:23"))
+                .bookingAdult(3)
+                .bookingChild(2)
+                .visitorName("이재혁")
+                .visitorPhoneNum("010-0000-0000")
+                .bookingPayType("카드")
+                .bookingPay(50000)
+                .bookingStartDate(LocalDate.parse("2024-06-01"))
+                .bookingEndDate(LocalDate.parse("2024-06-15"))
+                .roomId(1L)
+                .userId(1L)
+                .build();
+
+        System.out.println("[생성 전]" + booking);
+        bookingRepository.createBooking(booking);
+        System.out.println("[생성 후]" + booking);
+        System.out.println();
+        System.out.println("[예약 리스트]");
+        bookingRepository.findBooksByUserId(1L).forEach(System.out::println);
+    }
 
     @Test
     void bookingReadTest() {
