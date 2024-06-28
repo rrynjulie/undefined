4f87852d3a3236d683b358ec1ee41c77ef8ad9b0
jeongjunyeong <nejunyoung@gmail.com>
2024-06-28 09:18:13 +0900

    정준영_20240628_댓글추가기능 수정1

---
+++ src/main/java/com/lec/spring/domain/QryResult.java
@@ -0,0 +1,16 @@
+package com.lec.spring.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QryResult {
+    Integer count; // 결과값
+    String status; // 처리 결과 메세지
+}
