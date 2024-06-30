7443d94449183dbc6715546bea3137511b39270a
leemdongjoon2267 <dlaehdwns2020@naver.com>
2024-06-30 16:48:52 +0900

    임동준_20240630_오너 숙소, 객실 update 및 js, css 수정(2차)

--- src/main/resources/static/js/mypage/provider/ProvLodgingUpdate.js
+++ src/main/resources/static/js/mypage/provider/ProvLodgingUpdate.js
@@ -1,90 +1,20 @@
 $(document).ready(function() {
+
+    // 수정하기 버튼 클릭 시 처리
+    $('.btn2').on('click', function(event) {
+        event.preventDefault(); // 기본 폼 제출 동작 방지
+
+        if (confirm("수정을 하시겠습니까?")) {
+            alert("숙소 수정이 완료되었습니다.");
+            document.getElementById("updateForm").submit(); // 폼 제출
+        } else {
+            alert("숙소 수정이 취소되었습니다.");
+        }
+    });
+
     // 숙소 정보 로드 함수 호출
     loadLodgingInfo();
 
-    // 상위 지역에 따라 하위 지역 옵션 업데이트
-    const regionSelect = document.getElementById('region');
-    const subRegionSelect = document.getElementById('sub-region');
-
-    regionSelect.addEventListener('change', function() {
-        const selectedRegion = this.value;
-        updateSubRegionOptions(selectedRegion);
-    });
-
-    function updateSubRegionOptions(region) {
-        subRegionSelect.innerHTML = ''; // 기존 옵션 초기화
-
-        switch (region) {
-            case '서울특별시':
-                addOption('중구');
-                addOption('강남구');
-                addOption('동대문구');
-                addOption('서초구');
-                addOption('성북구');
-                addOption('노원구');
-                addOption('금천구');
-                addOption('용산구');
-                addOption('강동구');
-                addOption('강북구');
-                addOption('마포구');
-                break;
-            case '부산광역시':
-                addOption('해운대구');
-                break;
-            case '인천광역시':
-                addOption('강화군');
-                addOption('중구');
-                addOption('남동구');
-                break;
-            case '제주특별자치도':
-                addOption('제주시');
-                addOption('서귀포시');
-                break;
-            case '경기도':
-                addOption('수원시');
-                addOption('고양시');
-                addOption('용인시');
-                break;
-            case '경상남도':
-                addOption('창원시');
-                addOption('김해시');
-                break;
-            case '경상북도':
-                addOption('포항시');
-                addOption('경주시');
-                break;
-            case '전라남도':
-                addOption('여수시');
-                addOption('순천시');
-                break;
-            case '충청남도':
-                addOption('천안시');
-                addOption('아산시');
-                break;
-            case '강원도':
-                addOption('춘천시');
-                addOption('강릉시');
-                break;
-            case '대전광역시':
-                addOption('서구');
-                break;
-            case '대구광역시':
-                addOption('북구');
-                break;
-            case '울산광역시':
-                addOption('울주군');
-                break;
-            default:
-                addOption('하위 지역을 선택해 주세요.');
-        }
-    }
-
-    function addOption(subRegion) {
-        const option = document.createElement('option');
-        option.textContent = subRegion;
-        option.value = subRegion;
-        subRegionSelect.appendChild(option);
-    }
 
     // 숙소 정보 로드 함수
     function loadLodgingInfo() {
@@ -95,8 +25,7 @@ $(document).ready(function() {
         document.getElementById('lodgingType').value = lodging.lodgingType;
         document.getElementById('region').value = lodging.lodgingLocation1;
 
-        // 하위 지역 선택 옵션 업데이트
-        updateSubRegionOptions(lodging.lodgingLocation1);
+
         document.getElementById('sub-region').value = lodging.lodgingLocation2;
 
         document.getElementById('lodgingAddress').value = lodging.lodgingAddress;
--- src/main/resources/static/js/mypage/provider/ProvRoomRegister.js
+++ src/main/resources/static/js/mypage/provider/ProvRoomRegister.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const registrationForm = document.getElementById('registrationForm');
+    const registrationForm = document.getElementById('room-registration');
 
     registrationForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // 기본 제출 동작 막기
 
         const requiredFields = [
-            { id: 'roomname', label: '객실명' },
-            { id: 'normal', label: '객실 기준 인원수' },
-            { id: 'max', label: '객실 최대 인원수' },
-            { id: 'price', label: '객실 가격' },
-            // { id: 'grade', label: '객실 등급' },
-            { id: 'nm', label: '객실 호수' },
-            { id: 'area', label: '객실 면적' },
-            { id: 'bedcnt', label: '객실 침대 개수' },
-            { id: 'roombedtype', label: '객실 침대 유형' },
-            { id: 'bathcnt', label: '객실 욕실 개수' },
+            { id: 'roomName', label: '객실명' },
+            { id: 'roomNormalPeople', label: '객실 기준 인원수' },
+            { id: 'roomMaxPeople', label: '객실 최대 인원수' },
+            { id: 'roomPrice', label: '객실 가격' },
+            { id: 'roomNumber', label: '객실 호수' },
+            { id: 'roomArea', label: '객실 면적' },
+            { id: 'roomBed', label: '객실 침대 개수' },
+            { id: 'roomBedGrade', label: '객실 침대 유형' },
+            { id: 'roomBathroom', label: '객실 욕실 개수' }
             // { id: 'smoke', label: '객실 흡연 여부' },
             // { id: 'notion', label: '숙소 공지사항 및 소개' }
         ];
@@ -27,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (!input || input.value.trim() === '') {
                 allValid = false;
                 invalidFields.push(field.label);
-            } else if (['normal', 'max', 'price', 'area', 'bedcnt'].includes(field.id)) {
+            } else if (['roomNormalPeople', 'roomMaxPeople', 'roomPrice', 'roomArea', 'roomBed'].includes(field.id)) {
                 if (!isNumeric(input.value.trim())) {
                     allValid = false;
                     invalidFields.push(`${field.label}을(를) 숫자로 입력해 주세요.`);
@@ -36,55 +35,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         // 모든 필수 필드가 입력되지 않은 경우 처리
-        if (invalidFields.length === requiredFields.length) {
-            alert('모든 필수 항목을 입력(선택)해 주세요.');
-            return;
-        }
-
-        // 일부 필수 필드가 입력되지 않은 경우 처리
-        if (!allValid) {
-            const errorMessage = `아래 필수 항목을 입력(선택)해 주세요:\n- ${invalidFields.join('\n- ')}`;
+        if (invalidFields.length > 0) {
+            const errorMessage = `아래 필수 항목을 입력해 주세요:\n- ${invalidFields.join('\n- ')}`;
             alert(errorMessage);
             return;
         }
 
-        // 모든 필수 필드가 입력된 경우
-        alert('객실이 추가되었습니다.');
-        registrationForm.submit();
+        // 사용자에게 객실 등록 여부를 물어보는 confirm 대화상자 표시
+        const confirmed = confirm("객실을 등록하시겠습니까?");
+        if (confirmed) {
+            // 확인을 클릭한 경우 폼 제출
+            registrationForm.submit();
+        } else {
+            alert("객실 등록이 취소 되었습니다.");
+            return;
+        }
     });
 
     function isNumeric(value) {
         return /^\d+$/.test(value);
     }
 });
-
-
-
-// function uploadRoomImages() {
-//     var fileInput2 = document.getElementById('fileInput2');
-//     var formData = new FormData();
-//
-//     // 모든 선택된 파일들을 FormData에 추가
-//     for (var i = 0; i < fileInput2.files.length; i++) {
-//         formData.append('files', fileInput2.files[i]);
-//     }
-//
-//     // 객실 이미지를 업로드하는 AJAX 요청
-//     $.ajax({
-//         type: 'POST',
-//         url: '/mypage/provider/uploadRoomImages',  // 객실 이미지 업로드 처리하는 경로에 맞게 수정
-//         data: formData,
-//         contentType: false,
-//         processData: false,
-//         success: function(response) {
-//             console.log('객실 이미지 업로드 성공');
-//             console.log(response);  // 확인용 로그
-//
-//             alert('객실 이미지가 성공적으로 업로드되었습니다.');
-//         },
-//         error: function(err) {
-//             console.error('객실 이미지 업로드 에러');
-//             alert('객실 이미지 업로드 중 오류가 발생했습니다.');
-//         }
-//     });
-// }
