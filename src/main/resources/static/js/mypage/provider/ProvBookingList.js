d30620a3cc2f5b80b10824699be2118bd2f74513
JaeHyeokee <jaehyeok817@naver.com>
2024-06-30 22:28:03 +0900

    이재혁_20240630_판매자부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹부킹리스트

--- src/main/resources/static/js/mypage/provider/ProvBookingList.js
+++ src/main/resources/static/js/mypage/provider/ProvBookingList.js
@@ -1,8 +1,42 @@
-// 두 Select 불러오기
-const leftSelect = document.getElementById('lodging-select');
-const rightSelect = document.getElementById('room-select');
+// // 숙소를 선택했을 때 해당 숙소의 객실 정보를 객실 Select 요소의 Option 으로 불러오는 메소드
+// function getRooms(lodgingId) {
+//     let xmlHttpReq = new XMLHttpRequest();
+//     xmlHttpReq.open('GET', '/mypage/provider/ProvBookingList/rooms?lodgingId=' + lodgingId, true);
+//     xmlHttpReq.onload = function() {
+//         if(xmlHttpReq.status == 200) {
+//             let roomSelect = document.getElementById('room-select');  // 객실 select
+//             let fragment = document.createElement('div');  // 객실 select 내 option
+//             fragment.innerHTML = xmlHttpReq.responseText.trim();
+//             roomSelect.innerHTML = fragment.querySelector('#roomList').innerHTML;
+//         } else {
+//             console.log('Request failed. Returned status of ' + xmlHttpReq.status);
+//         }
+//     };
+//     xmlHttpReq.send();
+// }
+//
+// function getBooks(roomId) {
+//     let xmlHttpReq = new XMLHttpRequest();
+//     xmlHttpReq.open('GET', '/mypage/provider/ProvBookingList/books?roomId=' + roomId, true);
+//     xmlHttpReq.onload = function() {
+//         if(xmlHttpReq.status == 200) {
+//             let fragment = document.createElement('div');  // 객실 select 내 option
+//         }
+//     };
+//     xmlHttpReq.send();
+//     console.log('getRooms 왔다 갑니다 ~' + lodgingId);
+// }
 
-const option = {
-    [[${lodgings.lodgingId}]]: [
-    ]
-}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.btn-search').addEventListener('click', function(event) {
+        event.preventDefault();
+        const lodgingId = document.querySelector('#lodging-select').value;
+
+        fetch(`/mypage/provider/ProvBookingList/books?lodgingId=${lodgingId}`)
+            .then(response => response.text())
+            .then(data => {
+                document.querySelector('#booking-list').innerHTML = data;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+});
