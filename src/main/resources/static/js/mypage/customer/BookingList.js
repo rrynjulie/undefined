8f2fcdba7a71b4491d30741dbca6b7f227076f41
leemdongjoon2267 <dlaehdwns2020@naver.com>
2024-07-01 15:23:17 +0900

    임동준_20240701_후기 작성 중복 방지

--- src/main/resources/static/js/mypage/customer/BookingList.js
+++ src/main/resources/static/js/mypage/customer/BookingList.js
@@ -31,4 +31,13 @@ document.addEventListener("DOMContentLoaded", function() {
         const params = new URLSearchParams(new FormData(form)).toString();
         window.location.href = `${url}?${params}`;
     });
+
+    // 쿼리 매개변수를 확인하여 알림 표시
+    const urlParams = new URLSearchParams(window.location.search);
+    const alreadyPosted = urlParams.get('alreadyPosted');
+    if (alreadyPosted === 'true') {
+        alert("이미 후기를 작성했습니다.");
+    }
 });
+
+
