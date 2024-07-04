3f7f1ff703e5e4bdc3ce5b42a79ec7ccff2a3ddb
eunjeong0911 <eunjeong0911@naver.com>
2024-07-04 11:52:23 +0900

    최은정_20240704_예약취소의 취소

--- src/main/resources/static/js/mypage/customer/BookingList.js
+++ src/main/resources/static/js/mypage/customer/BookingList.js
@@ -1,31 +1,6 @@
-// // 리뷰 작성 모달 열기
-// function openReviewModal(hotelName) {
-//     var modal = document.getElementById('reviewModal');
-//     var modalTitle = document.getElementById('modalTitle');
-//     var hotelNameInput = document.getElementById('hotelName');
-//
-//     modal.style.display = 'block';
-//     modalTitle.textContent = '리뷰 작성하기 - ' + hotelName;
-//     hotelNameInput.value = hotelName;
-// }
-//
-// // 리뷰 작성 모달 닫기
-// function closeReviewModal() {
-//     var modal = document.getElementById('reviewModal');
-//     modal.style.display = 'none';
-// }
-//
-// // 모달 외부를 클릭하여 닫을 수 있도록 설정
-// window.onclick = function(event) {
-//     var modal = document.getElementById('reviewModal');
-//     if (event.target === modal) {
-//         modal.style.display = 'none';
-//     }
-// };
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("filter-form");
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
         const url = new URL(form.action);
         const params = new URLSearchParams(new FormData(form)).toString();
@@ -43,6 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
         sessionStorage.removeItem('alertShown');
     }
 });
+
+
+function confirmCancellation(event) {
+    if (!confirm("정말 예약을 취소하시겠습니까?")) {
+        event.preventDefault();
+    return false;
+}
+
+    return true;
+}
 
 
 
