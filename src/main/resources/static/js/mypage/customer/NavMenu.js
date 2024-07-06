834456d8fcec0f4c8992241226e93a986939a134
JaeHyeokee <jaehyeok817@naver.com>
2024-07-06 16:55:03 +0900

    이재혁_20240706_customerNavBar

--- src/main/resources/static/js/mypage/customer/NavMenu.js
+++ src/main/resources/static/js/mypage/customer/NavMenu.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.customer-menu-pc a');
-
-    function setActiveLink() {
-        links.forEach(link => {
-            if (link.href === window.location.href) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    }
-
-    setActiveLink();
-
-    links.forEach(link => {
-        link.addEventListener('click', function() {
-            links.forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const links = document.querySelectorAll('.customer-menu-pc a');
+//
+//     function setActiveLink() {
+//         links.forEach(link => {
+//             if (link.href === window.location.href) {
+//                 link.classList.add('active');
+//             } else {
+//                 link.classList.remove('active');
+//             }
+//         });
+//     }
+//
+//     setActiveLink();
+//
+//     links.forEach(link => {
+//         link.addEventListener('click', function() {
+//             links.forEach(link => link.classList.remove('active'));
+//             this.classList.add('active');
+//         });
+//     });
+// });
 
 //
 // document.getElementById('myLink').addEventListener('click', function(event) {
 //     event.preventDefault(); // Prevent the default link behavior
-//     document.getElementById('myImage').src = [[${/image/Booking.png}]]; // Change the image source
+//     document.getElementById('myImage').src = [[${/image/customer/BookingList.png}]]; // Change the image source
 // });
